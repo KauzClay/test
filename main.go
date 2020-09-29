780fc70a9621e99b7b44159f1b03a4cc8c150ccc
Clay Kauzlaric <ckauzlaric@vmware.com>
2020-09-29 17:21:24 +0000

    feat: takes commandline arguments

--- main.go
+++ main.go
@@ -1,55 +1,37 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
-	"net/http"
+	"os"
 
 	"github.com/golang-commonmark/markdown"
 )
 
-//snippet represents the snippet we will output.
-type snippet struct {
-	content string
-	lang    string
-}
-
-//getSnippet extract only code snippet from markdown object.
-func getSnippet(tok markdown.Token) snippet {
+func getFencedYAMLContent(tok markdown.Token) string {
 	switch tok := tok.(type) {
-	case *markdown.CodeBlock:
-		return snippet{
-			tok.Content,
-			"code",
-		}
-	case *markdown.CodeInline:
-		return snippet{
-			tok.Content,
-			"code inline",
-		}
 	case *markdown.Fence:
-		return snippet{
-			tok.Content,
-			tok.Params,
+		fmt.Println(tok)
+		if tok.Params == "yaml" {
+			return tok.Content
 		}
 	}
-	return snippet{}
+	return ""
 }
 
-//readFromWeb call the given url and return the content of the readme.
-func readFromWeb(url string) ([]byte, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	return ioutil.ReadAll(resp.Body)
+// creates file if it doesn't exist
+func writeToFile(name, content string) error {
+	data := []byte(content)
+	return ioutil.WriteFile(name, data, 0644)
 }
 
 func main() {
 
-	doc, err := ioutil.ReadFile("test.md")
+	input := os.Args[1]
+	output := flag.String("output-filename", "output.yml", "If not specified, will output to output.yml")
+
+	doc, err := ioutil.ReadFile(input)
 
 	if err != nil {
 		panic(err)
@@ -59,12 +41,15 @@ func main() {
 	md := markdown.New()
 	tokens := md.Parse(doc)
 
-	fmt.Println(len(tokens))
-	//Print the result
 	for _, t := range tokens {
-		snippet := getSnippet(t)
+		inlineCode := getFencedYAMLContent(t)
 
-		fmt.Println(snippet)
+		if inlineCode != "" {
+			err := writeToFile(*output, inlineCode)
+			if err != nil {
+				panic(err)
+			}
+		}
 	}
 
 }
